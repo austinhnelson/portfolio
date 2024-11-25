eb8b47930a3b0b6047f3dacd54bcd82df4c92753
Austin Nelson <nelsonhaustin@gmail.com>
2024-11-24 17:10:24 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Click me please!</Button>
+    </>
+  );
+}
+
+export default App;
