fbd78b1752938f7b7c0ae234bbe40d067e7397ab
Austin Nelson <nelsonhaustin@gmail.com>
2024-11-25 22:04:03 -0800

    Working on carousel

--- src/components/RightSidePanel.tsx
+++ src/components/RightSidePanel.tsx
@@ -13,7 +13,26 @@ export const RightSidePanel = () => {
     <ResizablePanel className="right-side-panel flex">
       <div className="flex flex-col w-2/4 p-4 space-y-4">
         <div className="about-me h-1/2 p-2 flex justify-center">About Me</div>
-        <div className="about-me h-1/2 p-2 flex justify-center"></div>
+        <div className="projects h-1/2 p-2 flex flex-col items-center justify-start">
+          <div className="p-1">Projects</div>
+          <Carousel className="p-4 w-full max-w-xs">
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem>
+                  <Card>
+                    <CardContent className="flex items-center justify-center">
+                      <div className="text-4xl font-semibold">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      </div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="text-black" />
+            <CarouselNext className="text-black" />
+          </Carousel>
+        </div>
       </div>
 
       <div className="flex-1 p-4">
