1ba22dddd2dab7c9efb97acb9a1fc0e5b2ac44a5
Austin Nelson <nelsonhaustin@gmail.com>
2024-11-25 21:22:21 -0800

    Flexbox on the right is working

--- src/components/RightSidePanel.tsx
+++ src/components/RightSidePanel.tsx
@@ -11,30 +11,14 @@ import { ResizablePanel } from "./ui/resizable";
 export const RightSidePanel = () => {
   return (
     <ResizablePanel className="right-side-panel flex">
-      <div className="flex flex-col w-1/3 p-4 space-y-4">
-        <div className="h-[400px]">About Me</div>
-        <Carousel className="h-[400px]">
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">
-                        {index + 1}
-                      </span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+      <div className="flex flex-col w-2/4 p-4 space-y-4">
+        <div className="about-me h-1/2 p-2 flex justify-center">About Me</div>
+        <div className="about-me h-1/2 p-2 flex justify-center"></div>
       </div>
 
-      <div className="flex-1 p-4">Experience</div>
+      <div className="flex-1 p-4">
+        <div className="experience h-full p-4">Experience</div>
+      </div>
     </ResizablePanel>
   );
 };
