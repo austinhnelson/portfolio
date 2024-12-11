681a80639b4d4294aa2612ed6e649768a3042ded
Austin Nelson <nelsonhaustin@gmail.com>
2024-12-10 22:44:22 -0800

    Fix CommonJS problem?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -53,4 +55,5 @@ export default {
       },
     },
   },
+  plugins: [tailwindcssAnimate],
 };
